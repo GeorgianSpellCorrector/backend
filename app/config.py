4f6bb7e94d6f92d520c4a73d24735f7e922d8289
demo <NamelessAlien1@gmail.com>
2023-07-09 18:48:39 +0400

    add settings for FastAPI and MongoDB

---
+++ app/config.py
@@ -0,0 +1,20 @@
+import os
+
+from pydantic.v1 import BaseSettings
+
+
+class FastAPISettings(BaseSettings):
+    app_name: str = 'Georgian Spell Checker'
+    version: str = '0.1.0'
+    description: str = 'Georgian Spell Checker API'
+
+
+class MongoDBSettings(BaseSettings):
+    MONGO_HOST: str = os.getenv('MONGO_HOST', 'localhost')
+    MONGO_PORT: int = int(os.getenv('MONGO_PORT', 27017))
+    MONGO_DB: str = os.getenv('MONGO_DB')
+    MONGO_USER: str = os.getenv('MONGO_USER')
+    MONGO_PASSWORD: str = os.getenv('MONGO_PASSWORD')
+    MONGO_URI: str = (
+        f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?retryWrites=true&w=majority&authSource=admin'
+    )
